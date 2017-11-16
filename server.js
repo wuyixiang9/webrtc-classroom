f7b9c39f416feb870dacd14909ff8db12b4f3dc2
wu.yixiang <clewyx@gmail.com>
2017-11-17 00:05:25 +0800

    添加session 但redis连接超时

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const http = require('http');
 const https = require('https');
 const path = require('path');
@@ -10,33 +9,38 @@ const views = require('koa-views');
 const uuidv4 = require('uuid/v4');
 const Router = require('koa-router');
 const enforceHttps = require('koa-sslify');
+const session = require('koa-session-redis');
+
 const routes = require('./routes');
 const store = require('./store');
 
 const router = new Router()
 
 const app = new Koa();
-const onlineUsers = new Set()
 
 routes(router)
+app.keys = ['room.limibee.com'];
+
+app.use(session({
+  maxAge: 1000 * 60 * 60 * 8,
+  store: {
+    host: 'v7.limibee.com',
+    port: 6379,
+    options: {
+      password: 'limibee.com'
+    }
+  },
+}));
 
 app.use(enforceHttps());
-app.use(async function (ctx, next) {
-  ctx.store = store
-  await next()
-});
+
 app.use(logger())
 // Must be used before any router is used
 app.use(require('koa-static')('./assets'));
 
 app.use(async function (ctx, next) {
-  let userId = ctx.cookies.get('userId')
-  if (!userId) {
-    userId = uuidv4()
-    ctx.cookies.set('userId', userId)
-  }
-  if (!onlineUsers.has(userId)) {
-    onlineUsers.add(userId)
+  if (!ctx.session.uid) {
+    ctx.session.uid = uuidv4()
   }
   await next()
 });
@@ -49,9 +53,9 @@ app.use(views(__dirname + '/views', {
 }));
 
 app.use(async function (ctx, next) {
-  let userId = ctx.cookies.get('userId')
+  let userId = ctx.session.uid
   const view = path.basename(ctx.originalUrl)
-  await ctx.render(view, {
+  await ctx.render('study', {
     userId,
     onlineUsers: Array.from(onlineUsers)
   });
